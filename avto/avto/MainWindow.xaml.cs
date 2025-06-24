648095a47932213fdac3e3c57826d787d6c0dde2
albertorozaliev <albertorozaliev06@gmail.com>
2025-06-24 23:24:18 +0300

    Add files via upload

---
+++ avto/avto/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using avto.avtoDataSetTableAdapters;
+
+
+namespace avto
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        typeModelTableAdapter typeModel = new typeModelTableAdapter();
+        ModelsTableAdapter models = new ModelsTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            typeModelDataGrid.ItemsSource = typeModel.GetData();
+            //typeModelComboBox.ItemsSource = typeModel.GetData();
+            //typeModelComboBox.DisplayMemberPath = "title";
+
+
+            ModelsDataGrid.ItemsSource = models.GetData();
+         
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ModelsDataGrid.Columns[0].Visibility = Visibility.Collapsed;
+            ModelsDataGrid.Columns[2].Visibility = Visibility.Collapsed;
+
+        }
+    }
+}
